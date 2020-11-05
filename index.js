0d2ee674f831b4fc68baab85a947e863a89f460d
Arunk28 <arunkumar280491@gmail.com>
2020-11-05 21:09:15 +0530

    in and out ask from console

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var faker = require('faker');
- 
-const fakersNeeded = 50
-const a = new Array(fakersNeeded).fill(null)
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  readline.question('How many fake data needed?', cnt => {
+    
+    const fakersNeeded = parseInt(cnt);
+    const a = new Array(fakersNeeded).fill(null)
                        .map(e =>
                    e = faker.fake("{{name.lastName}}, {{name.firstName}} {{internet.email}}"))
 
-console.log(a)                 ;
+    console.log(a)         
+
+  });
+
+        
 
 
 
